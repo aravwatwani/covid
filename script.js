3f894c8dc50ad2447e2f15c1e31910b7c82353cd
arav <aravwatwani@gmail.com>
2020-05-23 17:48:16 -0700

    added colored bubbles, drag force, outlines

--- script.js
+++ script.js
@@ -17,18 +17,25 @@
     .append("g")
     .attr("transform", "translate(0, 0)")
 
+  // Add a scale for bubble color
+  var myColor = d3.scaleOrdinal().domain(["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Minor Outlying Islands", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "U.S. Virgin Islands", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"])
+    .range(["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f", "#e5c494"]);
 
-
-  var radiusScale = d3.scaleSqrt().domain([1, 359235]).range([10, 70])
+  var radiusScale = d3.scaleSqrt().domain([1, 359235]).range([10, 85])
 
   // the simulation is a collection of forces
   // about how and where we want the bubbles
   var simulation = d3.forceSimulation()
     .force("x", d3.forceX(width / 2).strength(0.05))
-    .force("y", d3.forceY((height / 2) - 200).strength(0.05))
+    .force("y", d3.forceY((height / 2) - 150).strength(0.05))
     .force("collide", d3.forceCollide(function(d) {
-      return radiusScale(d.cases) + 1;
+      return radiusScale(d.cases) + 3;
     }))
+
+  var drag = d3.drag()
+    .on("start", dragStarted)
+    .on("drag", dragged)
+    .on("end", dragEnded);
 
   d3.queue()
     .defer(d3.csv, "05-20-2020.csv")
@@ -38,11 +45,14 @@
     var circles = svg.selectAll(".state")
       .data(datapoints)
       .enter().append("circle")
+      .call(drag)
       .attr("class", "state")
       .attr("r", function(d) {
         return radiusScale(d.cases)
       })
-      .attr("fill", "orange")
+      .style("fill", function(d) {
+        return myColor(d.state);
+      })
 
     simulation.nodes(datapoints)
       .on("tick", ticked)
@@ -56,6 +66,24 @@
           return d.y
         })
     }
+  }
+
+  function dragStarted(d) {
+    simulation.alphaTarget(0.3).restart();
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragged(d) {
+    simulation.alphaTarget(0.5);
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragEnded(d) {
+    simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
   }
 
 })();
